ed21c4045952ef261d44b486386f533847e4dc2c
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-15 09:58:59 +0700

    math

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// func main() {
+// 	m1 := 2
+// 	m2 := 3
+// 	fmt.Println(m1 + m2)
+// }
 
 func main() {
-	fmt.Println("Hello World")
+	m1 := "My name"
+	m1 = "name"
+	fmt.Println(strings.ReplaceAll(m1, "m", "NO"))
 }

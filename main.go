639dba8546b9c1d9263e703a5c4d8eca221da07d
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-20 03:09:18 +0700

    add mongoo

--- main.go
+++ main.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-// func main() {
-// 	m1 := 2
-// 	m2 := 3
-// 	fmt.Println(m1 + m2)
-// }
-
-// func main() {
-// 	m1 := "My name"
-// 	m1 = "name"
-// 	fmt.Println(strings.Split(m1, ""))
-// }
-func main() {
-	todo()
+func close(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
-func todo() {
-	// var arr [] int
-	arr := []int{1, 2, 3, 4}
-	fmt.Println(arr)
+
+func connect(uri string) (*mongo.Client, context.Context,
+	context.CancelFunc, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+func ping(client *mongo.Client, ctx context.Context) error {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected successfully")
+	return nil
+}
+
+func main() {
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer close(client, ctx, cancel)
+	ping(client, ctx)
 }
